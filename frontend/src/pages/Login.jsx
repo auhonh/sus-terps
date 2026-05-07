5dde73d7dbf930fb9c9d9d1f52d96b316312a273
GracelynRuth <ruthgracelyn@gmail.com>
2026-05-07 16:30:00 -0400

    testing login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,58 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login(){
     const navigate = useNavigate();
+
+    const[username, setUsername] = useState("");
+    const[password, setPassword] = useState("");
+
+     const handleLogin = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Login successful");
+        navigate("/userstats");
+      } else {
+        alert(data.detail);
+      }
+
+    } catch (error) {
+      console.error(error);
+      alert("Server error");
+    }
+  };
 
     return (
     <div id="login">
         <h1>Login</h1>
 
         <input
-            placeholder="Enter your email"
+            placeholder="Username"
+            value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <input
             type="password"
             placeholder="Password"
+            value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button onClick={() => navigate("/userstats")}>Login</button>
+        <button onClick={handleLogin}>Login</button>
     </div>
     )
 }
